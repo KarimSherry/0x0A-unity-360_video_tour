1170edfd179358c9195889f62ac84d2d58948050
KarimSherry <karimedusherry@outlook.com>
2021-11-24 21:15:14 +0200

    commit

---
+++ Assets/Scripts/Boards.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boards : MonoBehaviour
+{
+    //LivingRoom boards
+    public GameObject WhiteBoard;
+    public GameObject Library;
+    
+    //Cantina boards
+    public GameObject CantinaMessage;
+    
+    //Cube boards
+    public GameObject CubeMessage;
+
+    //Mezzanine boards
+    public GameObject BoothMessage;
+    public GameObject ImacsMessage;
+    
+    
+    public void openWhiteBoard()
+	{
+		if (WhiteBoard.activeSelf)
+			WhiteBoard.SetActive(false);
+		else
+			WhiteBoard.SetActive(true);
+	}
+     public void openlibrary()
+	{
+		if (Library.activeSelf)
+			Library.SetActive(false);
+		else
+			Library.SetActive(true);
+	}
+
+    public void openCantinaMessage()
+	{
+		if (CantinaMessage.activeSelf)
+			CantinaMessage.SetActive(false);
+		else
+			CantinaMessage.SetActive(true);
+	}
+    public void openCubeMessage()
+	{
+		if (CubeMessage.activeSelf)
+			CubeMessage.SetActive(false);
+		else
+			CubeMessage.SetActive(true);
+	}
+    public void openBoothMessage()
+	{
+		if (BoothMessage.activeSelf)
+			BoothMessage.SetActive(false);
+		else
+			BoothMessage.SetActive(true);
+	}
+    public void openImacsMessage()
+	{
+		if (ImacsMessage.activeSelf)
+			ImacsMessage.SetActive(false);
+		else
+			ImacsMessage.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigation : MonoBehaviour
+{
+    public GameObject VRcam;
+
+public void goToLivingRoom()
+{
+    VRcam.transform.position = new Vector3(0, 0, 0);
+} 
+public void goToCantina()
+{
+    VRcam.transform.position = new Vector3(40, 0, 0);
+} 
+public void goToCube()
+{
+    VRcam.transform.position = new Vector3(20, 0, 0);
+} 
+public void goToMezzanine()
+{
+    VRcam.transform.position = new Vector3(60, 0, 0);
+} 
+
+
+}
